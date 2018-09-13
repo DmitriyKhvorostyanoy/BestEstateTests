ed7931045577f477d50b2586ee9c2922628f64b7
dmikh <dmi.khv246@gmail.com>
2018-09-13 11:34:48 +0300

    two working tests(pdf and form)

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -1,4 +1,31 @@
 package tests;
 
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.BeforeClass;
+import pages.MainPage;
+import pages.PropertyPage;
+import pages.SearchResultPage;
+
+import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.WebDriverRunner.clearBrowserCache;
+
 public class BaseTest {
+    static MainPage mainPage;
+    static SearchResultPage searchResultPage;
+    static PropertyPage propertyPage;
+
+    @BeforeClass
+    public static void setUp() {
+        mainPage = new MainPage();
+        searchResultPage = new SearchResultPage();
+        propertyPage = new PropertyPage();
+
+        SelenideLogger.addListener("allure", new AllureSelenide());
+        System.setProperty("selenide.browser", "chrome");
+        clearBrowserCache();
+        Configuration.timeout = 15000;
+        open("https://best-estate.net/");
+    }
 }
