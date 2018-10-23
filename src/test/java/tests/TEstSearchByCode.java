6601abe94e83bf88d893f018c023f51e63a08640
DmitriyKhvorostyanoy <dmi.khv246@gmail.com>
2018-10-23 12:59:00 +0300

    description for test "Search by code"

--- src/test/java/tests/TEstSearchByCode.java
+++ src/test/java/tests/TEstSearchByCode.java
@@ -1,11 +1,18 @@
 package tests;
 
 import com.codeborne.selenide.Condition;
+import io.qameta.allure.Description;
+import io.qameta.allure.Severity;
+import io.qameta.allure.SeverityLevel;
+import io.qameta.allure.junit4.DisplayName;
 import org.junit.Test;
 
 import static junit.framework.TestCase.assertEquals;
 
 public class TEstSearchByCode extends BaseTest {
+    @DisplayName("Поиск по коду недвижимости")
+    @Description("вводим в поле поиска в хедере валидный код и на  страице результатов поиска смотрим чтоб было найдена одна карточка с таким кодом")
+    @Severity(SeverityLevel.CRITICAL)
     @Test
     public void searchByCodeOfProperty() {
         header.fillAndSubmitSearchField("32654");//код самого дорого проперти на сайте
